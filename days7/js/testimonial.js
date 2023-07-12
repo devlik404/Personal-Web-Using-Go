5ce59cedf09577c9171de5215913174d0568c42b
Malik fajar <devfajarmalik@gmail.com>
2023-07-12 12:32:42 +0700

    deploy days7 ratting class menggunakan OOP

--- days7/js/testimonial.js
+++ days7/js/testimonial.js
@@ -1,31 +1,64 @@
 class Rate {
-    image = ""
-    quote = ""
-    name =""
-
-    constructor(image,quote,name){
-        this.image = image
-        this.quote = quote
-        this.name = name
-
+    #quote = ""
+    #image = ""
+ 
+    constructor(quote,image){
+       this.#quote = quote
+        this.#image = image
+       
+    }
+    get quote(){
+      return this.#quote
+    }
+    get image(){
+      return this.#image
+    }
+    get autor() {
+      throw new Error('nama user harus di isi cokk')
+    }
+    get testHTML(){
+      return `
+      <div class="card">
+      <img src="${this.image}" alt="Avatar" style="width:100%">
+      <div class="container">
+        <i>"${this.quote}"</i> 
+        <h4><b>${this.autor}</b></h4> 
+      </div>
+      </div>
+      `
     }
 }
-const test1 = new Rate('https://images.unsplash.com/photo-1686896435653-4a110ea92c84?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEzfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60','wah sangat keren yah gais','- fathul izhar')
-const test2 = new Rate('https://images.unsplash.com/photo-1687444846017-1b4fd96a2743?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80','ini merupakan era dunia baru','- sasa irahi')
-const test3 = new Rate('https://images.unsplash.com/photo-1686944716358-0fc13acb8266?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=938&q=80','dunia digital sangat membantu masyarakat berinteraksi lebih dalam','- oba chan')
+class testiRate extends Rate{
+#autor = ""
+constructor(autor,quote,image){
+  super(quote,image)
+  this.#autor = autor
+}
+get autor(){
+  return "pengguna:" + this.#autor
+}
+}
+
+class companyTest extends Rate{
+  #company =""
+  constructor(company,quote,image){
+    super(quote,image)
+    this.#company = company
+  }
+
+  get autor(){
+    return "company:" + this.#company
+  }
+}
+const test1 = new testiRate("- ikeh chan","kimochi desu","https://images.unsplash.com/photo-1686896435653-4a110ea92c84?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEzfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60")
+const test2 = new testiRate('- miku',"sugggoi","https://images.unsplash.com/photo-1687444846017-1b4fd96a2743?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80")
+const test3 = new companyTest("- pemuda pancasila","dimana ada rendang disitu ada kami","https://images.unsplash.com/photo-1686944716358-0fc13acb8266?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=938&q=80")
+
 
 let testimonial = [test1, test2, test3]
-let testHTML = ''
+let testHTML = ""
 
 for(let i = 0;i < testimonial.length; i++){
-testHTML += `
-<div class="card">
-<img src="${testimonial[i].image}" alt="Avatar" style="width:100%">
-<div class="container">
-  <i>"${testimonial[i].quote}"</i> 
-  <h4><b>${testimonial[i].name}</b></h4> 
-</div>
-</div>
-`
+testHTML += testimonial[i].testHTML
 }
 document.getElementById('container-card').innerHTML = testHTML;
